edbca54064b8a6c8de3bd5538071dcadecc24aff
Ahmed Fathi <98648216+ahmedfathy112@users.noreply.github.com>
2024-04-28 18:11:39 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -37,155 +37,149 @@ fetch(apiUrl)
   })
   .catch((error) => console.error("Error fetching data:", error));
 
-// //   sports news section
-// let sportsSec = document.getElementById("sports-sec");
-// const topic = "sports";
-// const apiUrl2 = `https://newsapi.org/v2/top-headlines?q=${encodeURIComponent(
-//   topic
-// )}&country=us&apiKey=${apiKey}`;
+//   sports news section
+let sportsSec = document.getElementById("sports-sec");
+const topic = "sports";
+const apiUrl2 = `https://newsapi.org/v2/top-headlines?q=${topic}&country=us&apiKey=${apiKey}`;
 
-// // استدعاء الوظيفة لجلب البيانات
-// fetch(apiUrl2)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     // عمل لوب على البيانات المسترجعة
-//     data.articles.forEach((article) => {
-//       // استخراج البيانات المطلوبة
-//       const sourceNameSports = article.source?.name;
-//       const titleSports = article.title;
-//       const imageSports = article.urlToImage;
-//       const urlSports = article.url;
-//       const descriptionSports = article.description;
-//       const publishTimeSports = article.publishedAt;
+// استدعاء الوظيفة لجلب البيانات
+fetch(apiUrl2)
+  .then((response) => response.json())
+  .then((data) => {
+    // عمل لوب على البيانات المسترجعة
+    data.articles.forEach((article) => {
+      // استخراج البيانات المطلوبة
+      const sourceNameSports = article.source?.name;
+      const titleSports = article.title;
+      const imageSports = article.urlToImage;
+      const urlSports = article.url;
+      const descriptionSports = article.description;
+      const publishTimeSports = article.publishedAt;
 
-//       if (
-//         sourceNameSports &&
-//         titleSports &&
-//         imageSports &&
-//         urlSports &&
-//         descriptionSports &&
-//         publishTimeSports
-//       ) {
-//         sportsSec.innerHTML += `
-//       <div class="card">
-//           <img src="${imageSports}" alt="" />
-//           <h2 class="author">from :<span>${sourceNameSports}</span></h2>
-//           <p class="time">Puplish Time : ${publishTimeSports}</p>
-//           <a href="${urlSports}" target= "_blank" class="news-link">
-//           <h2 class="title">
-//             ${titleSports}
-//           </h2>
-//           </a>
-//           <p class="description">
-//             ${descriptionSports}
-//           </p>
-//         </div>
-//       `;
-//       }
-//     });
-//   })
-//   .catch((error) => console.error("Error fetching data:", error));
-// // start tech news
-// const techSec = document.getElementById("tech-sec");
-// const topic2 = "tech";
-// const apiUrl3 = `https://newsapi.org/v2/top-headlines?q=${encodeURIComponent(
-//   topic2
-// )}&country=us&apiKey=${apiKey}`;
+      if (
+        sourceNameSports &&
+        titleSports &&
+        imageSports &&
+        urlSports &&
+        descriptionSports &&
+        publishTimeSports
+      ) {
+        sportsSec.innerHTML += `
+      <div class="card">
+          <img src="${imageSports}" alt="" />
+          <h2 class="author">from :<span>${sourceNameSports}</span></h2>
+          <p class="time">Puplish Time : ${publishTimeSports}</p>
+          <a href="${urlSports}" target= "_blank" class="news-link">
+          <h2 class="title">
+            ${titleSports}
+          </h2>
+          </a>
+          <p class="description">
+            ${descriptionSports}
+          </p>
+        </div>
+      `;
+      }
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
+// start tech news
+const techSec = document.getElementById("tech-sec");
+const topic2 = "tech";
+const apiUrl3 = `https://newsapi.org/v2/top-headlines?q=${topic2}&country=us&apiKey=${apiKey}`;
 
-// // استدعاء الوظيفة لجلب البيانات
-// fetch(apiUrl3)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     // عمل لوب على البيانات المسترجعة
-//     data.articles.forEach((article) => {
-//       // استخراج البيانات المطلوبة
-//       const sourceNameTech = article.source?.name;
-//       const titleTech = article.title;
-//       const imageTech = article.urlToImage;
-//       const urlTech = article.url;
-//       const descriptionTech = article.description;
-//       const publishTimeTech = article.publishedAt;
+// استدعاء الوظيفة لجلب البيانات
+fetch(apiUrl3)
+  .then((response) => response.json())
+  .then((data) => {
+    // عمل لوب على البيانات المسترجعة
+    data.articles.forEach((article) => {
+      // استخراج البيانات المطلوبة
+      const sourceNameTech = article.source?.name;
+      const titleTech = article.title;
+      const imageTech = article.urlToImage;
+      const urlTech = article.url;
+      const descriptionTech = article.description;
+      const publishTimeTech = article.publishedAt;
 
-//       if (
-//         sourceNameTech &&
-//         titleTech &&
-//         imageTech &&
-//         urlTech &&
-//         descriptionTech &&
-//         publishTimeTech
-//       ) {
-//         techSec.innerHTML += `
-//       <div class="card">
-//           <img src="${imageTech}" alt="" />
-//           <h2 class="author">from :<span>${sourceNameTech}</span></h2>
-//           <p class="time">Puplish Time : ${publishTimeTech}</p>
-//           <a href="${urlTech}" target= "_blank" class="news-link">
-//           <h2 class="title">
-//             ${titleTech}
-//           </h2>
-//           </a>
-//           <p class="description">
-//             ${descriptionTech}
-//           </p>
-//         </div>
-//       `;
-//       }
-//     });
-//   })
-//   .catch((error) => console.error("Error fetching data:", error));
+      if (
+        sourceNameTech &&
+        titleTech &&
+        imageTech &&
+        urlTech &&
+        descriptionTech &&
+        publishTimeTech
+      ) {
+        techSec.innerHTML += `
+      <div class="card">
+          <img src="${imageTech}" alt="" />
+          <h2 class="author">from :<span>${sourceNameTech}</span></h2>
+          <p class="time">Puplish Time : ${publishTimeTech}</p>
+          <a href="${urlTech}" target= "_blank" class="news-link">
+          <h2 class="title">
+            ${titleTech}
+          </h2>
+          </a>
+          <p class="description">
+            ${descriptionTech}
+          </p>
+        </div>
+      `;
+      }
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
 
-// // start life sec
-// const lifeSec = document.getElementById("life-sec");
-// const topic3 = "life";
-// const apiUrl4 = `https://newsapi.org/v2/top-headlines?q=${encodeURIComponent(
-//   topic3
-// )}&country=us&apiKey=${apiKey}`;
+// start life sec
+const lifeSec = document.getElementById("life-sec");
+const topic3 = "life";
+const apiUrl4 = `https://newsapi.org/v2/top-headlines?q=${topic3}&country=us&apiKey=${apiKey}`;
 
-// // استدعاء الوظيفة لجلب البيانات
-// fetch(apiUrl4)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     data.articles.forEach((article) => {
-//       const sourceNameLife = article.source?.name;
-//       const titleLife = article.title;
-//       const imageLife = article.urlToImage;
-//       const urlLife = article.url;
-//       const descriptionLife = article.description;
-//       const publishTimeLife = article.publishedAt;
+// استدعاء الوظيفة لجلب البيانات
+fetch(apiUrl4)
+  .then((response) => response.json())
+  .then((data) => {
+    data.articles.forEach((article) => {
+      const sourceNameLife = article.source?.name;
+      const titleLife = article.title;
+      const imageLife = article.urlToImage;
+      const urlLife = article.url;
+      const descriptionLife = article.description;
+      const publishTimeLife = article.publishedAt;
 
-//       if (
-//         sourceNameLife &&
-//         titleLife &&
-//         imageLife &&
-//         urlLife &&
-//         descriptionLife &&
-//         publishTimeLife
-//       ) {
-//         lifeSec.innerHTML += `
-//       <div class="card">
-//           <img src="${imageLife}" alt="" />
-//           <h2 class="author">from :<span>${sourceNameLife}</span></h2>
-//           <p class="time">Puplish Time : ${publishTimeLife}</p>
-//           <a href="${urlLife}" target= "_blank" class="news-link">
-//           <h2 class="title">
-//             ${titleLife}
-//           </h2>
-//           </a>
-//           <p class="description">
-//             ${descriptionLife}
-//           </p>
-//         </div>
-//       `;
-//       }
-//     });
-//   })
-//   .catch((error) => console.error("Error fetching data:", error));
+      if (
+        sourceNameLife &&
+        titleLife &&
+        imageLife &&
+        urlLife &&
+        descriptionLife &&
+        publishTimeLife
+      ) {
+        lifeSec.innerHTML += `
+      <div class="card">
+          <img src="${imageLife}" alt="" />
+          <h2 class="author">from :<span>${sourceNameLife}</span></h2>
+          <p class="time">Puplish Time : ${publishTimeLife}</p>
+          <a href="${urlLife}" target= "_blank" class="news-link">
+          <h2 class="title">
+            ${titleLife}
+          </h2>
+          </a>
+          <p class="description">
+            ${descriptionLife}
+          </p>
+        </div>
+      `;
+      }
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
 
 // const apiKey5 = "78e82696e81b45aca23fa75797f7f379";
-// const topic5 = "life";
+// const topic5 = "sport";
 // const apiUrl5 = `https://newsapi.org/v2/top-headlines?q=${encodeURIComponent(
 //   topic5
-// )}&country=us&apiKey=${apiKey5}`;
+// )}&country=eg&apiKey=${apiKey5}`;
 
 // fetch(apiUrl5)
 //   .then((response) => response.json())
